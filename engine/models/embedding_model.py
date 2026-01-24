038357f3d58a50633bd60bae1cac743e360d4331
Dinesh Selvam <dinesh@Dineshs-MacBook-Pro.local>
2026-01-24 21:20:16 +0400

    Embedding Model class initialsation with vector embedding generation function

---
+++ engine/models/embedding_model.py
@@ -0,0 +1,35 @@
+class EmbeddingModel():
+    
+    def __init__(self):
+
+        import mlx.core as mx
+        from mlx_embeddings.utils import load
+
+        self.model_name = "mlx-community/Qwen3-Embedding-0.6B-4bit-DWQ"
+        self.mx = mx
+        self.model,self.tokenizer = load(self.model_name)
+
+    def get_vector_embeddings(self,text:str)->list:
+
+        tokens = self.mx.array(self.tokenizer.encode(text))
+        tokens = tokens[None, :] 
+
+        output = self.model(tokens)
+
+        hidden_states = output.last_hidden_state
+
+        embedding = hidden_states[:, -1, :]
+
+        norm = self.mx.linalg.norm(embedding,axis=-1,keepdims=True)
+        normalized_vector = embedding / self.mx.maximum(norm, 1e-12)
+    
+        return normalized_vector
+
+
+model = EmbeddingModel()
+caption = "MAIN SUBJECTS: A vintage red car. SPATIAL LAYOUT: Centered. ENVIRONMENT: Sunset."
+vector = model.get_vector_embeddings(caption)
+
+print(f"Vector Dimensions: {vector.shape[1]}") # 1024 for 0.6B model
+print(f"First 5 values: {vector[0, :5]}")
+
