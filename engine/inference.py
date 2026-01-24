4e9c1846a1170aaf439be8afb13b5ad59a90792a
Dinesh Selvam <dinesh@Dineshs-MacBook-Pro.local>
2026-01-24 20:20:54 +0400

    Inference Class intialisation with image index and image caption generation methods

--- engine/inference.py
+++ engine/inference.py
@@ -1,3 +1,5 @@
+import os
+from models.models import VlmModel,MlxModel
 class Inference: 
 
     def __init__(self):
@@ -5,14 +7,31 @@ class Inference:
 
     
     def get_index(self,dir_path:str):
-        
-        import os
 
         image_info = []
         for root,dir,files in os.walk(dir_path, topdown=True):
             for name in files: 
                 if name.endswith((".jpg",".png",".jpeg")):
                     image_path = root + "/" + name
-                    image_info.append({"iamge_path" :image_path,"image_file_name":name})
-    
+                    image_info.append({"image_path" :image_path,"image_filename":name})
+        
+        return image_info 
 
+    def get_image_captions(self,model:MlxModel,image_indexes:list)->list:
+
+        captions = []
+        for i in image_indexes:
+            image_path = i["image_path"]
+            response = model.generate(image_path)
+            captions.append(response.text)
+            break
+
+        print(captions)
+        return captions
+
+
+# inf = Inference()
+# model = MlxModel()
+
+# print(image_info,type(image_info))
+# inf.get_image_captions(model,image_info)
